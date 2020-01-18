c0b764bd966a9fbc03ee1b9c56d49456f5d785f6
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2020-01-18 19:56:02 -0200

    chore: first batch before first phase

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from 'react-native';
+
+// eslint-disable  @typescript-eslint/no-var-requires
+const React = require('react');
+
+const App: React.FC = () =>
+{
+	return (
+		<View>
+			<Text>beep</Text>
+		</View>
+	);
+};
+
+export default App;
