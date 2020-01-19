9de0e9bbb201eb076ea2f2e491de21d3cfa8587e
Alexandre Cavalheiro S. Tiago Da Silva <alexandre.cssilva@gmail.com>
2020-01-19 21:35:20 -0200

    fix(App.tsx): correctly disable no-var-requires

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 
-// eslint-disable  @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const React = require('react');
 
 const App: React.FC = () =>
